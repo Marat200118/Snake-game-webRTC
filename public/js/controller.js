f25278ab13ab5b9b5d65b0815af6666af856a388
Marat200118 <marat.samigullin1@gmail.com>
2024-03-28 22:04:42 +0100

    finally figured out the webrtc logic and connection method

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,136 +1,56 @@
 let socket;
-let peerConnection;
-let dataChannel;
-let controlMethod = "";
 let peer;
-// let targetSocketId = getUrlParameter("id");
+let targetSocketId = new URLSearchParams(window.location.search).get("id");
+
+const servers = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
 
 const init = () => {
-  targetSocketId = getUrlParameter("id");
-  if (!targetSocketId) {
-    alert("Missing target ID in querystring");
-    return;
-  }
   initSocket();
 };
 
 const initSocket = () => {
   socket = io.connect("/");
-
   socket.on("connect", () => {
-    console.log(`Controller Connected: ${socket.id}`);
-    setupWebRTC();
+    console.log(socket.id);
+    callPeer();
   });
 
-  socket.on("signal", (signal, fromPeerId) => {
-    // Only process the signal if it's meant for this controller
-    if (fromPeerId === targetSocketId) {
-      console.log("Received signal");
-      if (!peer) setupWebRTC();
-      peer.signal(signal);
-    }
+  socket.on("signal", async (myId, signal, peerId) => {
+    console.log(`Received signal from ${peerId}`);
+    console.log(signal);
+    peer.signal(signal);
+  });
+
+  socket.on("client-disconnect", (client) => {
+    console.log(`Client disconnected: ${client.id}`);
   });
 };
 
-const setupWebRTC = () => {
+const callPeer = async () => {
   peer = new SimplePeer({
     initiator: true,
     trickle: false,
+    config: servers,
   });
 
   peer.on("signal", (data) => {
-    console.log("Sending signal to", targetSocketId);
-    // Correctly include targetSocketId in the signal emission
     socket.emit("signal", targetSocketId, data);
   });
 
   peer.on("connect", () => {
-    console.log("Peer connection established");
-    // Now connected to the game, can send game control signals
-  });
-
-  peer.on("data", (data) => {
-    console.log("Received data:", new TextDecoder().decode(data));
-    // Process incoming data, such as game state updates
-  });
-
-  peer.on("close", () => console.log("Connection closed"));
-  peer.on("error", (err) => console.error("Error:", err));
-};
-
-// Handling of incoming data
-const handleIncomingData = (data) => {
-  const parsedData = JSON.parse(data);
-  console.log("Data received:", parsedData);
-};
-
-const setupControlMethodListeners = () => {
-  document.getElementById("useGyroscope").addEventListener("click", () => {
-    controlMethod = "gyroscope";
-    document.querySelector(".gyroscope-controls").style.display = "block";
-    document.querySelector(".button-controls").style.display = "none";
-    setupGyroscopeControlListeners();
-  });
-
-  document.getElementById("useButtons").addEventListener("click", () => {
-    controlMethod = "buttons";
-    document.querySelector(".button-controls").style.display = "block";
-    document.querySelector(".gyroscope-controls").style.display = "none";
-    setupButtonControlListeners();
-  });
-};
-
-const setupGyroscopeControlListeners = () => {
-  if ("Gyroscope" in window) {
-    const gyroscope = new Gyroscope({ frequency: 60 });
-    gyroscope.addEventListener("reading", (e) => {
-      if (gyroscope.x > 0.2) {
-        sendCommand("right");
-      } else if (gyroscope.x < -0.2) {
-        sendCommand("left");
-      }
-
-      if (gyroscope.y > 0.2) {
-        sendCommand("down");
-      } else if (gyroscope.y < -0.2) {
-        sendCommand("up");
-      }
+    console.log("Peer connected, now you can send messages.");
+    // Now safe to add the click event listener
+    document.getElementById("btnPress").addEventListener("click", () => {
+      console.log("Sending 'Button Pressed'");
+      peer.send("Button Pressed");
     });
-    gyroscope.start();
-  } else {
-    console.log("Gyroscope not supported by the browser");
-  }
-};
-
-const setupButtonControlListeners = () => {
-  // Setup buttons listeners for manual control
-  document
-    .querySelector(".up")
-    .addEventListener("click", () => sendCommand("up"));
-  document
-    .querySelector(".down")
-    .addEventListener("click", () => sendCommand("down"));
-  document
-    .querySelector(".left")
-    .addEventListener("click", () => sendCommand("left"));
-  document
-    .querySelector(".right")
-    .addEventListener("click", () => sendCommand("right"));
-};
-
-const sendCommand = (command) => {
-  if (peer && peer.connected) {
-    peer.send(JSON.stringify({ command: command }));
-  }
-};
-
-const getUrlParameter = (name) => {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  const regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-  const results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
+  });
 };
 
 init();
--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,15 @@
 const $url = document.getElementById("url");
 const $canvas = document.getElementById("gameCanvas");
+const $gameCanvas = document.getElementById("gameCanvas");
 const $game = document.querySelector(".game");
-const $score = document.querySelector(".score");
 const $connectionInfo = document.querySelector(".connection-information");
 const $instructions = document.querySelector(".greeting-and-instructions");
+const $controlmethod = document.querySelector(".controlmethod");
 const $startmessage = document.querySelector(".start-message");
 const ctx = $canvas.getContext("2d");
 
-let socket;
-let targetSocketId;
-
 let gameHasStarted = false;
+let socket;
 let dx = 10;
 let dy = 0;
 let changingDirection = false;
@@ -18,7 +17,6 @@ let foodX;
 let foodY;
 let score = 0;
 let gameLoop;
-let peer;
 
 const servers = {
   iceServers: [
@@ -29,225 +27,67 @@ const servers = {
 };
 
 const init = () => {
-  // targetSocketId = getUrlParameter("id");
-  // if (!targetSocketId) {
-  //   alert(`Missing target ID in querystring`);
-  //   return;
-  // }
   initSocket();
-  // createPeer(true, targetSocketId);
   $game.style.display = "none";
-  $connectionInfo.style.display = "flex";
 };
 
 const initSocket = () => {
-  socket = io.connect("/");
-  socket.on("connect", () => {
-    console.log(`Connected: ${socket.id}`);
-    const url = `${new URL(
-      `/controller.html?id=${socket.id}`,
-      window.location
-    )}`;
-    $url.textContent = url;
-    $url.setAttribute("href", url);
-    const typeNumber = 4;
-    const errorCorrectionLevel = "L";
-    const qr = qrcode(typeNumber, errorCorrectionLevel);
-    qr.addData(url);
-    qr.make();
-    document.getElementById("qr").innerHTML = qr.createImgTag(4);
+  socket = io.connect(`/`);
+  socket.on(`connect`, () => {
+    console.log(socket.id);
+    linkandQr();
   });
 
-  socket.on("signal", (signal, fromPeerId) => {
-    console.log("Received signal from", fromPeerId, signal);
-    if (!peer) {
-      createPeer();
+  socket.on("signal", async (myId, signal, peerId) => {
+    console.log(`Received signal from ${peerId}`);
+    console.log(signal);
+    if (signal.type === "offer") {
+      answerPeerOffer(myId, signal, peerId);
     }
-    // Directly pass the signal data to peer.signal without parsing as JSON
     peer.signal(signal);
   });
 
-  socket.on("controller-disconnect", () => {
-    console.log("Controller has disconnected.");
-    // Add any UI updates or game reset logic here
-    alert("Controller has disconnected. Please refresh to start a new game.");
+  socket.on("client-disconnect", (client) => {
+    console.log(`Client disconnected: ${client.id}`);
+    $instructions.style.display = "flex";
+    $controlmethod.style.display = "none";
     $game.style.display = "none";
+    $startmessage.style.display = "none";
     $connectionInfo.style.display = "flex";
-    $instructions.style.display = "block";
-    // Optionally, reset the game to its initial state
-    resetGame(); // Ensure you have a function defined to reset the game
   });
 };
 
-const createPeer = () => {
-  peer = new SimplePeer({ initiator: false });
-
+const answerPeerOffer = async (myId, offer, peerId) => {
+  peer = new SimplePeer();
   peer.on("signal", (data) => {
-    console.log("Emitting signal");
-    // The signal is now emitted without a specific target since this is the receiver part
-    socket.emit("signal", data);
+    socket.emit("signal", peerId, data);
   });
 
   peer.on("connect", () => {
-    console.log("Peer connection established");
-    // Now ready to receive controller commands
+    console.log("Peer connected, now you can send messages.");
+    $instructions.style.display = "none";
+    $controlmethod.style.display = "block";
+    $game.style.display = "flex";
+    $startmessage.style.display = "block";
+    $connectionInfo.style.display = "none";
   });
 
   peer.on("data", (data) => {
-    handleGameCommand(JSON.parse(new TextDecoder().decode(data)));
-    // Process incoming control commands
-  });
-
-  peer.on("close", () => console.log("Connection closed"));
-  peer.on("error", (err) => console.error("Error:", err));
-};
-
-const handleIncomingData = (data) => {
-  const message = data.toString();
-  console.log("Received data:", message);
-
-  if (message === "start game") {
-    startGame();
-  }
-};
-
-let snake = [
-  { x: 150, y: 150 },
-  { x: 140, y: 150 },
-  { x: 130, y: 150 },
-  { x: 120, y: 150 },
-  { x: 110, y: 150 },
-];
-
-const main = () => {
-  if (hasGameEnded()) return;
-
-  changingDirection = false;
-  clearTimeout(gameLoop);
-  gameLoop = setTimeout(function onTick() {
-    clearCanvas();
-    drawFood();
-    drawSnake();
-    moveSnake();
-
-    if (snake[0].x === foodX && snake[0].y === foodY) {
-      score += 10;
-      document.querySelector(".score").innerHTML = `Score: ${score}`;
-      socket.emit("scoreUpdate", { score: score });
-      const newHead = { x: snake[0].x + dx, y: snake[0].y + dy };
-      snake.unshift(newHead);
-      createFood();
-    }
-
-    main();
-  }, 150);
-};
-
-const clearCanvas = () => {
-  ctx.fillStyle = "black";
-  ctx.strokestyle = "white";
-  ctx.fillRect(0, 0, $canvas.width, $canvas.height);
-  ctx.strokeRect(0, 0, $canvas.width, $canvas.height);
-};
-
-const drawSnake = () => {
-  snake.forEach((part) => {
-    ctx.fillStyle = "lightgreen";
-    ctx.strokestyle = "darkgreen";
-    ctx.fillRect(part.x, part.y, 10, 10);
-    ctx.strokeRect(part.x, part.y, 10, 10);
+    console.log("Data from controller:", data.toString());
   });
 };
 
-const moveSnake = () => {
-  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-  snake.unshift(head);
-  snake.pop();
-};
+const linkandQr = () => {
+  const url = `${new URL(`/controller.html?id=${socket.id}`, window.location)}`;
+  $url.textContent = url;
+  $url.setAttribute("href", url);
 
-const changeDirection = (command) => {
-  if (changingDirection) return;
-  changingDirection = true;
-
-  if (command === "up" && dy === 0) {
-    dx = 0;
-    dy = -10;
-  } else if (command === "down" && dy === 0) {
-    dx = 0;
-    dy = 10;
-  } else if (command === "left" && dx === 0) {
-    dx = -10;
-    dy = 0;
-  } else if (command === "right" && dx === 0) {
-    dx = 10;
-    dy = 0;
-  }
-};
-
-const hasGameEnded = () => {
-  for (let i = 4; i < snake.length; i++) {
-    const collided = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
-    if (collided) return true;
-  }
-  const hitLeftWall = snake[0].x < 0;
-  const hitRightWall = snake[0].x > $canvas.width - 10;
-  const hitToptWall = snake[0].y < 0;
-  const hitBottomWall = snake[0].y > $canvas.height - 10;
-
-  if (hitLeftWall || hitRightWall || hitToptWall || hitBottomWall) {
-    socket.emit("scoreUpdate", { score });
-    console.log("Game over");
-    return true;
-  }
-
-  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
-};
-
-const createFood = () => {
-  foodX = Math.round((Math.random() * ($canvas.width - 10)) / 10) * 10;
-  foodY = Math.round((Math.random() * ($canvas.height - 10)) / 10) * 10;
-  snake.forEach(function isFoodOnSnake(part) {
-    const foodIsOnSnake = part.x == foodX && part.y == foodY;
-    if (foodIsOnSnake) createFood();
-  });
-};
-
-const drawFood = () => {
-  ctx.fillStyle = "red";
-  ctx.strokestyle = "darkred";
-  ctx.fillRect(foodX, foodY, 10, 10);
-  ctx.strokeRect(foodX, foodY, 10, 10);
-};
-
-const startGame = () => {
-  if (!gameHasStarted) {
-    gameHasStarted = true;
-    score = 0;
-    document.querySelector(".score").innerHTML = "Score: 0";
-    createFood();
-    main();
-  }
-};
-
-const resetGame = () => {
-  console.log("Resetting game");
-  clearTimeout(gameLoop);
-  snake = [
-    { x: 150, y: 150 },
-    { x: 140, y: 150 },
-    { x: 130, y: 150 },
-    { x: 120, y: 150 },
-    { x: 110, y: 150 },
-  ];
-  dx = 10;
-  dy = 0;
-  score = 0;
-  gameHasStarted = true;
-  document.querySelector(".score").innerHTML = "Score: 0";
-  clearCanvas();
-  createFood();
-  drawSnake();
-  main();
+  const typeNumber = 4;
+  const errorCorrectionLevel = "L";
+  const qr = qrcode(typeNumber, errorCorrectionLevel);
+  qr.addData(url);
+  qr.make();
+  document.getElementById("qr").innerHTML = qr.createImgTag(4);
 };
 
 init();
